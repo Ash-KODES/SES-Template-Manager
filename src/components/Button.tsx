f4fc288d59460f99caffbee0f0765d2f79dbae10
Shivam <singhshivamkr@gmail.com>
2023-03-20 18:55:44 +0530

    :bug: Make variant optional

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { HTMLAttributes } from "preact/compat";
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   label: string;
-  variant: "primary" | "secondary";
+  variant?: "primary" | "secondary";
 }
 
 const Button = ({ label, className, variant = "primary", ...rest }: Props) => {
