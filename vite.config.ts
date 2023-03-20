4913e8fe7e748df7c729dff2b99385f3d3cca8a2
onam <onamkrverma@gmail.com>
2023-03-20 17:43:59 +0530

    :sparkles: Add vite alias plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
+import { ViteAliases } from "vite-aliases";
 import preact from "@preact/preset-vite";
-
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  resolve: {
-    alias: {
-      "@css": path.resolve(__dirname, "./src/css"),
-    },
-  },
+  plugins: [preact(), ViteAliases()],
 });
