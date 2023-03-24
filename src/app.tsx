4d6d0e31cb7676845d72ef9ab3976b5de26b6fbf
onam <onamkrverma@gmail.com>
2023-03-24 13:19:33 +0530

    :sparkles: add protected route

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,33 @@
 import "@css/index.css";
-import { Router, Route } from "preact-router";
+import {
+  Router,
+  Route,
+  RouterOnChangeArgs,
+  getCurrentUrl,
+} from "preact-router";
 import Home from "@page/Home";
 import Auth from "@page/Auth";
+import AddNewTemplate from "@page/AddNewTemplate";
+import { useState } from "preact/hooks";
 
 export function App() {
+  let [currentUrl, setcurrentUrl] = useState(getCurrentUrl());
+  const localData = JSON.parse(localStorage.getItem("auth") as string);
+
+  const publicRoute = ["/auth"];
+
+  if (!publicRoute.includes(currentUrl) && localData === null) {
+    setcurrentUrl("/auth");
+  }
+
   return (
-    <Router>
+    <Router
+      url={currentUrl}
+      onChange={(e: RouterOnChangeArgs) => setcurrentUrl(e.url)}
+    >
       <Route path="/" component={Home} />
       <Route path="/auth" component={Auth} />
+      <Route path="/new" component={AddNewTemplate} />
     </Router>
   );
 }
