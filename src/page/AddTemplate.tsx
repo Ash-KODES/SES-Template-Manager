18501033167dc40d194646693689c1b08e1d8210
onam <onamkrverma@gmail.com>
2023-03-29 15:27:55 +0530

    :sparkles: add button loading and placeholder text

--- src/page/AddTemplate.tsx
+++ src/page/AddTemplate.tsx
@@ -1,3 +1,4 @@
+import { Placeholder } from "@/constant/placeholders";
 import { CreateTemplateSchema } from "@/schema/forms-schema";
 import { addNewTemplate, setupSesClient } from "@api/ses";
 import Button from "@components/Button";
@@ -5,14 +6,16 @@ import Input from "@components/Input";
 import TextArea from "@components/TextArea";
 import "@css/addTemplate.css";
 import { route } from "preact-router";
-import { ChangeEvent, useRef } from "preact/compat";
+import { ChangeEvent, useRef, useState } from "preact/compat";
 
 const AddTemplate = () => {
   const localData = JSON.parse(localStorage.getItem("auth") as string);
   const formRef = useRef<HTMLFormElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleCreate = async (e: ChangeEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
       if (localData !== null) {
         const { accessKeyId, secretAccessKey } = localData;
@@ -22,20 +25,23 @@ const AddTemplate = () => {
         const formData = new FormData(formRef.current);
         const formDataObj = Object.fromEntries(formData.entries());
         const parsedFormVal = CreateTemplateSchema.parse(formDataObj);
-        const { templateName, templateText, tempalteHtml } = parsedFormVal;
-        console.log(tempalteHtml, templateName, templateText);
+        const { templateName, templateSubject, templateText, tempalteHtml } =
+          parsedFormVal;
+        console.log(templateName, templateSubject, templateText, tempalteHtml);
         const TemplateContent = {
           Html: tempalteHtml,
-          Subject: templateText,
+          Subject: templateSubject,
           Text: templateText,
         };
         const TemplateName = templateName;
         await addNewTemplate({ TemplateContent, TemplateName });
         console.log("template created success");
+        setIsLoading(false);
         route("/");
       }
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
     }
   };
 
@@ -47,19 +53,30 @@ const AddTemplate = () => {
           type="text"
           label="Template name"
           name="templateName"
-          placeholder={"My coolest cold email"}
+          placeholder={Placeholder.TemplateName}
+        />
+        <Input
+          type="text"
+          label="Template subject"
+          name="templateSubject"
+          placeholder={Placeholder.templateSubject}
         />
         <TextArea
           label="Template Text"
           name="templateText"
-          placeholder={"Write here template name"}
+          placeholder={Placeholder.TemplateText}
         />
         <TextArea
           label="Template HTML"
           name="tempalteHtml"
-          placeholder={"Write here template html"}
+          placeholder={Placeholder.TemplateHTML}
         />
-        <Button type="submit" label="Create Template" />
+        <Button
+          type="submit"
+          className={isLoading ? "btn-loading" : ""}
+          disabled={isLoading}
+          label={`${isLoading ? "" : "Create Template"}`}
+        />
       </form>
     </div>
   );
