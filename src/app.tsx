8141a7c5865615f5cf37695e5993d0a0c86613ce
onam <onamkrverma@gmail.com>
2023-04-04 17:27:39 +0530

    :sparkles: add edit component

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,8 @@ import Home from "@page/Home";
 import Auth from "@page/Auth";
 import AddTemplate from "@page/AddTemplate";
 import { useState } from "preact/hooks";
+import EditTemplate from "@page/EditTemplate";
+import { setupSesClient } from "@api/ses";
 
 export function App() {
   let [currentUrl, setcurrentUrl] = useState(getCurrentUrl());
@@ -20,6 +22,11 @@ export function App() {
     setcurrentUrl("/auth");
   }
 
+  if (localData !== null) {
+    const { accessKeyId, secretAccessKey } = localData;
+    setupSesClient({ accessKeyId, secretAccessKey });
+  }
+
   return (
     <Router
       url={currentUrl}
@@ -28,6 +35,7 @@ export function App() {
       <Route path="/" component={Home} />
       <Route path="/auth" component={Auth} />
       <Route path="/new" component={AddTemplate} />
+      <Route path="/edit/:templateName" component={EditTemplate} />
     </Router>
   );
 }
---
+++ src/page/EditTemplate.tsx
@@ -0,0 +1,93 @@
+import Button from "@components/Button";
+import Input from "@components/Input";
+import TextArea from "@components/TextArea";
+import { useEffect, useRef, useState } from "preact/hooks";
+import "@css/editTemplate.css";
+import { setupSesClient, updateTemplate } from "@api/ses";
+import { ChangeEvent } from "preact/compat";
+import { CreateTemplateSchema } from "@/schema/forms-schema";
+import { route } from "preact-router";
+import useGetTemplate from "@/hooks/useGetTemplate";
+interface Props {
+  templateName: string;
+}
+
+const EditTemplate = ({ templateName }: Props) => {
+  const localData = JSON.parse(localStorage.getItem("auth") as string);
+  const formRef = useRef<HTMLFormElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const data = useGetTemplate(templateName);
+  console.log(data);
+  console.log(data.res);
+
+  const handleUpdate = async (e: ChangeEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      if (localData !== null) {
+        const { accessKeyId, secretAccessKey } = localData;
+        setupSesClient({ accessKeyId, secretAccessKey });
+      }
+      if (formRef.current) {
+        const formData = new FormData(formRef.current);
+        const formDataObj = Object.fromEntries(formData.entries());
+        const parsedFormVal = CreateTemplateSchema.parse(formDataObj);
+        const { templateName, templateSubject, templateText, tempalteHtml } =
+          parsedFormVal;
+        console.log(templateName, templateSubject, templateText, tempalteHtml);
+        const TemplateContent = {
+          Html: tempalteHtml,
+          Subject: templateSubject,
+          Text: templateText,
+        };
+        const TemplateName = templateName;
+        await updateTemplate({ TemplateContent, TemplateName });
+        console.log("template update success");
+        setIsLoading(false);
+        route("/");
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="edit-template-container">
+      <form onSubmit={handleUpdate} ref={formRef}>
+        <h2>Edit your Email template</h2>
+        <Input
+          type="text"
+          label="Template name"
+          name="templateName"
+          value={templateName}
+        />
+        <Input
+          type="text"
+          label="Template subject"
+          name="templateSubject"
+          value={data.res?.Subject}
+        />
+        <TextArea
+          label="Template Text"
+          name="templateText"
+          value={data.res?.Text}
+        />
+        <TextArea
+          label="Template HTML"
+          name="tempalteHtml"
+          value={data.res?.Html}
+        />
+        <Button
+          type="submit"
+          className={isLoading ? "btn-loading" : ""}
+          disabled={isLoading}
+          label={`${isLoading ? "" : "Save changes"}`}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default EditTemplate;
