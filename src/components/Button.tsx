eca7901fd8e309ebf67ba5bfb75534f1cc9d33e7
Shivam <singhshivamkr@gmail.com>
2023-04-04 19:40:36 +0530

    :sparkles: Create isLoading prop with signal

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,31 @@
+import { computed, ReadonlySignal } from "@preact/signals";
 import { HTMLAttributes } from "preact/compat";
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   label: string;
   variant?: "primary" | "secondary";
+  isLoading?: ReadonlySignal<boolean>;
 }
 
-const Button = ({ label, className, variant = "primary", ...rest }: Props) => {
+const Button = ({
+  label,
+  isLoading,
+  className,
+  variant = "primary",
+  ...rest
+}: Props) => {
+  console.log("button rendered");
+
+  const btnLabel = computed(() => (!isLoading?.value ? label : ""));
+  const btnClass = computed(
+    () =>
+      `btn ${variant} ${isLoading?.value ? "btn-loading" : ""} ${
+        className ?? ""
+      }`
+  );
   return (
-    <button className={`btn ${variant} ${className ?? ""}`} {...rest}>
-      {label}
+    <button className={btnClass.value} {...rest}>
+      {btnLabel}
     </button>
   );
 };
