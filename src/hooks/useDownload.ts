cb630e6c238b488ee0552987a1c0c192f7f6f48f
onam <onamkrverma@gmail.com>
2023-04-07 16:21:16 +0530

    :sparkles: created download hooks

---
+++ src/hooks/useDownload.ts
@@ -0,0 +1,43 @@
+import { getTemplate } from "@api/ses";
+import { signal } from "@preact/signals";
+import FileSaver from "file-saver";
+
+let { Html, Subject, Text } = {
+  Html: "",
+  Subject: "",
+  Text: "",
+};
+let isLoading = signal(true);
+let error = signal<null | string>(null);
+
+const useDownload = () => {
+  const downloadFn = async (TemplateName: string) => {
+    try {
+      const response = await getTemplate({ TemplateName });
+      // console.log(response);
+
+      Html = response.TemplateContent?.Html!;
+      Subject = response.TemplateContent?.Subject!;
+      Text = response.TemplateContent?.Text!;
+
+      const htmlBlob = new Blob([Html], {
+        type: "text/plain;charset=utf-8",
+      });
+      FileSaver.saveAs(htmlBlob, `${TemplateName}.html`);
+      const textBlob = new Blob([Text], {
+        type: "text/plain;charset=utf-8",
+      });
+      FileSaver.saveAs(textBlob, `${TemplateName}.txt`);
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err.message;
+      }
+    } finally {
+      isLoading.value = false;
+    }
+    return { isLoading, error };
+  };
+  return downloadFn;
+};
+
+export default useDownload;
