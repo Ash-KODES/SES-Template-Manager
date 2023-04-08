48a0bd5246ee4523dac49fb4f2dad20d49dd3fc2
onam <onamkrverma@gmail.com>
2023-04-08 19:10:57 +0530

    :hammer: add template data component

--- src/page/AddTemplate.tsx
+++ src/page/AddTemplate.tsx
@@ -1,9 +1,5 @@
-import { Placeholder } from "@/constant/placeholders";
 import useAddTemplate from "@/hooks/useAddTemplate";
-import Button from "@components/Button";
-import Input from "@components/Input";
-import TextArea from "@components/TextArea";
-import "@css/addTemplate.css";
+import TemplateData from "@components/TemplateData";
 import { signal } from "@preact/signals";
 import { route } from "preact-router";
 import { ChangeEvent, useRef } from "preact/compat";
@@ -12,46 +8,24 @@ const isLoading = signal(false);
 
 const AddTemplate = () => {
   const formRef = useRef<HTMLFormElement>(null);
-  const getAdd = useAddTemplate();
+  const addTemplate = useAddTemplate();
 
   const handleCreate = async (e: ChangeEvent) => {
     e.preventDefault();
     isLoading.value = true;
-    const addTemp = await getAdd({ formRef });
-    console.log("done", addTemp);
+    const addTemp = await addTemplate({ formRef });
+    console.log(addTemp);
     isLoading.value = false;
     route("/");
   };
 
   return (
-    <div className="add-template-container">
-      <form onSubmit={handleCreate} ref={formRef}>
-        <h2>Create your Email template</h2>
-        <Input
-          type="text"
-          label="Template name"
-          name="templateName"
-          placeholder={Placeholder.TemplateName}
-        />
-        <Input
-          type="text"
-          label="Template subject"
-          name="templateSubject"
-          placeholder={Placeholder.templateSubject}
-        />
-        <TextArea
-          label="Template Text"
-          name="templateText"
-          placeholder={Placeholder.TemplateText}
-        />
-        <TextArea
-          label="Template HTML"
-          name="tempalteHtml"
-          placeholder={Placeholder.TemplateHTML}
-        />
-        <Button type="submit" isLoading={isLoading} label={"Create Template"} />
-      </form>
-    </div>
+    <TemplateData
+      formRef={formRef}
+      handleSubmit={handleCreate}
+      variant="create"
+      isLoading={isLoading}
+    />
   );
 };
 
--- src/page/EditTemplate.tsx
+++ src/page/EditTemplate.tsx
@@ -1,12 +1,9 @@
-import Button from "@components/Button";
-import Input from "@components/Input";
-import TextArea from "@components/TextArea";
 import { useRef } from "preact/hooks";
 import { ChangeEvent } from "preact/compat";
 import { route } from "preact-router";
 import useGetTemplate from "@/hooks/useGetTemplate";
-import "@css/editTemplate.css";
 import useUpdateTemplate from "@/hooks/useUpdateTemplate";
+import TemplateData from "@components/TemplateData";
 
 interface Props {
   templateName: string;
@@ -14,14 +11,13 @@ interface Props {
 
 const EditTemplate = ({ templateName }: Props) => {
   const formRef = useRef<HTMLFormElement>(null);
-  const getUpdate = useUpdateTemplate();
-
+  const updateTemplate = useUpdateTemplate();
   const { data, isLoading } = useGetTemplate(templateName);
   // console.log(data.res);
 
   const handleUpdate = async (e: ChangeEvent) => {
     e.preventDefault();
-    const updateData = await getUpdate({ formRef });
+    const updateData = await updateTemplate({ formRef });
     console.log(updateData);
     route("/");
   };
@@ -29,27 +25,14 @@ const EditTemplate = ({ templateName }: Props) => {
   // console.log(isLoading.value);
 
   return (
-    <div className="edit-template-container">
-      <form onSubmit={handleUpdate} ref={formRef}>
-        <h2>Edit your Email template</h2>
-        <Input
-          type="text"
-          label="Template name"
-          name="templateName"
-          value={templateName}
-        />
-        <Input
-          type="text"
-          label="Template subject"
-          name="templateSubject"
-          value={data.Subject}
-          disabled={isLoading}
-        />
-        <TextArea label="Template Text" name="templateText" value={data.Text} />
-        <TextArea label="Template HTML" name="tempalteHtml" value={data.Html} />
-        <Button type="submit" isLoading={isLoading} label="Save changes" />
-      </form>
-    </div>
+    <TemplateData
+      templateName={templateName}
+      data={data}
+      isLoading={isLoading}
+      formRef={formRef}
+      variant="edit"
+      handleSubmit={handleUpdate}
+    />
   );
 };
 
