d336c20a2e4912c8716938d20b19c798a7b33ed4
onam <onamkrverma@gmail.com>
2023-04-10 18:35:20 +0530

    :hammer: update hook for multiple files download

--- src/hooks/useDownload.ts
+++ src/hooks/useDownload.ts
@@ -5,20 +5,22 @@ import FileSaver from "file-saver";
 const isLoading = signal(true);
 const error = signal<null | string>(null);
 
-const downloadTemplates = async ([TemplateName]: string[]) => {
+const downloadTemplates = async (TemplateNameArr: string[]) => {
   try {
-    const response = await getTemplate({ TemplateName });
-    // console.log(response);
-    const Html = response.TemplateContent?.Html!;
-    const Text = response.TemplateContent?.Text!;
-    const htmlBlob = new Blob([Html], {
-      type: "text/plain;charset=utf-8",
+    TemplateNameArr?.map(async (TemplateName) => {
+      const response = await getTemplate({ TemplateName });
+      // console.log(response);
+      const Html = response.TemplateContent?.Html!;
+      const Text = response.TemplateContent?.Text!;
+      const htmlBlob = new Blob([Html], {
+        type: "text/plain;charset=utf-8",
+      });
+      FileSaver.saveAs(htmlBlob, `${TemplateName}.html`);
+      const textBlob = new Blob([Text], {
+        type: "text/plain;charset=utf-8",
+      });
+      FileSaver.saveAs(textBlob, `${TemplateName}.txt`);
     });
-    FileSaver.saveAs(htmlBlob, `${TemplateName}.html`);
-    const textBlob = new Blob([Text], {
-      type: "text/plain;charset=utf-8",
-    });
-    FileSaver.saveAs(textBlob, `${TemplateName}.txt`);
   } catch (err) {
     if (err instanceof Error) {
       error.value = err.message;
