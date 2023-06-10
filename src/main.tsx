4c5c49e9763ba4f37fa381cc8daae5bfef5c89cf
Shivam <singhshivamkr@gmail.com>
2023-06-11 00:44:43 +0530

    :sparkles: add uno css

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { render } from "preact";
+import "virtual:uno.css";
 import { App } from "./app";
 
 render(<App />, document.getElementById("app") as HTMLElement);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { ViteAliases } from "vite-aliases";
+import UnoCSS from "unocss/vite";
 import preact from "@preact/preset-vite";
 import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), ViteAliases(), removeConsole()],
+  plugins: [UnoCSS(), preact(), ViteAliases(), removeConsole()],
 });
